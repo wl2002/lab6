0eea5a06d7b29004e657420563059607d0d63c70
William Lewis <wrlewis@ucsd.edu>
2015-02-13 21:56:35 +0000

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,7 +27,24 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + idNumber, addproject);
 }
+4
+function addproject(result){
+
+	//console.log(result);
+
+	var projectHTML = '<img src="' + result['image'] + '" class="detailsImage"/>' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p>' +
+		result['summary'];
+
+	var details = $("#project" + result['id'] + " .details");
+	details.html(projectHTML);
+
+
+} 
 
 /*
  * Make an AJAX call to retrieve a color palette for the site
@@ -35,4 +52,20 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	e.preventDefault();
+	$.get("/palette", randomColor);
+
+}
+
+function randomColor(result){
+
+	//console.log(result);
+	//console.log(result.colors);
+	var colors = result.colors.hex;
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);
 }
